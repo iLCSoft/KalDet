b38372f28f985c2b83f6e0245a32fef47ab3f90d
Frank Gaede <frank.gaede@desy.de>
2010-11-24 14:58:41 +0000

    fixed units to new default units of KalTest for lengths and Bfield : cm and Tesla (from K.Fujii)

--- lctpc/lp1jgem/EXTPCKalDetector.cxx
+++ lctpc/lp1jgem/EXTPCKalDetector.cxx
@@ -12,6 +12,8 @@
 //* (Update Recored)
 //*   2009/11/23  K.Ikematsu   Derived from KalTest/examples/kaltest/
 //*                                         hybrid/tpc/EXTPCKalDetector.cxx
+//*   2010/11/17  K.Fujii      Changed unit system to (mm, nsec, T) 
+//*                            from (cm, nsec, kG)
 //*
 //* $Id: EXTPCKalDetector.cxx,v 1.4 2010-09-22 05:49:59 fujiik Exp $
 //*************************************************************************
@@ -43,7 +45,11 @@
 #include "TVirtualPad.h"
 
 EXTPCKalDetector * EXTPCKalDetector::fgInstance = 0;
-Double_t EXTPCKalDetector::fgVdrift = 7.6e-3;
+#ifdef OLD_UNIT_SYSTEM
+Double_t EXTPCKalDetector::fgVdrift = 7.6e-3; // [cm/nsec]
+#else
+Double_t EXTPCKalDetector::fgVdrift = 76.e-3; // [mm/nsec]
+#endif
 
 ClassImp(EXTPCKalDetector)
 
@@ -86,7 +92,11 @@ EXTPCKalDetector::EXTPCKalDetector(Int_t m)
     modules.push_back(&theTPCParameters.getModule(i));
   }
 
+#ifdef OLD_UNIT_SYSTEM
   static const Double_t kmm2cm = 0.1;
+#else
+  static const Double_t kmm2cm = 1.;
+#endif
 
   static const Double_t lhalf
     = theTPCParameters.getMaxDriftLength() * kmm2cm;     // half length
@@ -96,14 +106,14 @@ EXTPCKalDetector::EXTPCKalDetector(Int_t m)
   //////////////////////////////////////////////////////////
   static const Double_t neff    = 22.7;
 #if 1
-  static const Double_t sigmax0 = 38.3e-4;
-  static const Double_t sigmax1 = 101.5e-4 / TMath::Sqrt(10.) / TMath::Sqrt(neff);
-  static const Double_t sigmaz0 = 500.e-4;
-  static const Double_t sigmaz1 = 154.e-4  / TMath::Sqrt(10.) / TMath::Sqrt(neff);
+  static const Double_t sigmax0 = 38.3e-3 * kmm2cm;
+  static const Double_t sigmax1 = 101.5e-3 / TMath::Sqrt(10.) * TMath::Sqrt(kmm2cm) / TMath::Sqrt(neff);
+  static const Double_t sigmaz0 = 500.e-3 * kmm2cm;
+  static const Double_t sigmaz1 = 154.e-3  / TMath::Sqrt(10.) * TMath::Sqrt(kmm2cm) / TMath::Sqrt(neff);
 #else
-  static const Double_t sigmax0 = 100.e-4;
+  static const Double_t sigmax0 = 100.e-3 * kmm2cm;
   static const Double_t sigmax1 = 0.;
-  static const Double_t sigmaz0 = 500.e-4;
+  static const Double_t sigmaz0 = 500.e-3 * kmm2cm;
   static const Double_t sigmaz1 = 0.;
 #endif
 
