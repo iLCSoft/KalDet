41052da0a59ae786fa5c26d18b90735d713c71e2
Steven Aplin <steven.aplin@desy.de>
2011-11-29 14:39:11 +0000

    added calo face to support detector

---
+++ ild/common/ILDPolygonBarrelMeasLayer.cc
@@ -0,0 +1,188 @@
+
+#include "ILDPolygonBarrelMeasLayer.h"
+#include "ILDPlanarHit.h"
+
+
+#include "TVTrack.h"
+#include "TVector3.h"
+#include "TMath.h"
+#include "TRotMatrix.h"
+#include "TBRIK.h"
+#include "TNode.h"
+#include "TString.h"
+
+#include <EVENT/TrackerHitPlane.h>
+
+#include <math.h>
+#include <assert.h>
+#include <algorithm>
+
+#include "streamlog/streamlog.h"
+
+
+
+
+ILDPolygonBarrelMeasLayer::ILDPolygonBarrelMeasLayer(TMaterial &min,
+                                                     TMaterial &mout,
+                                                     double   Bz,
+                                                     double   sortingPolicy,
+                                                     double   r0,       // min distance to the z-axis
+                                                     double   lhalf,    // half length
+                                                     int      nsides,   
+                                                     double   zpos,     // z of the centre 
+                                                     double   phi0,     // phi of the first normal following the xaxis positive rotation
+                                                     std::vector<int>      CellIDs,                                                     
+                                                     bool     is_active,
+                                                     const Char_t    *name) : 
+ILDVMeasLayer(min, mout, Bz, CellIDs, is_active, name),
+_r0(r0),_lhalf(lhalf),_nsides(nsides),_zpos(zpos),_phi0(phi0)
+
+{
+    
+  _segment_dphi = 2.0*M_PI / _nsides; 
+  
+  phi0 = angular_range_2PI(phi0);
+  
+  _start_phi = phi0 - 0.5*_segment_dphi;
+ 
+  _start_phi = angular_range_2PI(_start_phi);
+
+  for (int i = 0; i < _nsides; ++i) {
+    
+    const TVector3  center(r0*cos(phi0),r0*sin(phi0),zpos);
+    const TVector3  normal(center);
+    double width = 2*(_r0*tan(_segment_dphi*0.5));
+    
+    double phi = i*_segment_dphi+_start_phi;
+    
+    ILDParallelPlanarMeasLayer p(min,mout,r0,phi,Bz,sortingPolicy,width,2*lhalf,0.0,false);
+    _planes.push_back(p);
+    
+  }
+
+  
+  _rmax = r0*sin((_r0*tan(_segment_dphi*0.5)));
+  
+  _enclosing_cylinder = new  TCylinder(_rmax, lhalf, 0., 0., 0.);
+  
+  // now check for constistency
+
+
+//  if( true ) {
+//    streamlog_out(ERROR) << "ILDPolygonBarrelMeasLayer::ILDPolygonBarrelMeasLayer trapezoids overlaps: exit(1) called from " << __FILE__ << "   line " << __LINE__ << std::endl; 
+//    exit(1);
+//  }
+  
+}
+
+
+TKalMatrix ILDPolygonBarrelMeasLayer::XvToMv(const TVector3 &xv) const
+{
+  
+  // Calculate measurement vector (hit coordinates) from global coordinates:
+  int index = this->get_plane_index(xv.Phi());
+  
+  return _planes[index].XvToMv(xv);
+  
+}
+
+
+TVector3 ILDPolygonBarrelMeasLayer::HitToXv(const TVTrackHit &vht) const
+{
+  
+  streamlog_out(ERROR) << "ILDPolygonBarrelMeasLayer::HitToXv Not implemented: exit(1) called from " << __FILE__ << "   line " << __LINE__ << std::endl; 
+  exit(1);
+
+  return TVector3(0.,0.,0.);
+  
+}
+
+void ILDPolygonBarrelMeasLayer::CalcDhDa(const TVTrackHit &vht,
+                                         const TVector3   &xxv,
+                                         const TKalMatrix &dxphiada,
+                                         TKalMatrix &H)  const
+{
+
+  streamlog_out(ERROR) << "ILDPolygonBarrelMeasLayer::CalcDhDa Not implemented: exit(1) called from " << __FILE__ << "   line " << __LINE__ << std::endl; 
+  exit(1);
+  
+}
+
+
+Int_t ILDPolygonBarrelMeasLayer::CalcXingPointWith(const TVTrack  &hel,
+                                                    TVector3 &xx,
+                                                    Double_t &phi,
+                                                    Int_t     mode,
+                                                    Double_t  eps) const{
+
+  _enclosing_cylinder->CalcXingPointWith(hel, xx, phi, mode);
+  
+  int index = this->get_plane_index(xx.Phi());
+  
+  _planes[index].CalcXingPointWith(hel,xx,phi,mode,eps);
+  
+  return (IsOnSurface(xx) ? 1 : 0);
+  
+  
+}
+
+Bool_t ILDPolygonBarrelMeasLayer::IsOnSurface(const TVector3 &xx) const
+{
+  
+  //  std::cout << "IsOnSurface " << std::endl;  
+  int index = this->get_plane_index(xx.Phi());
+  
+  return _planes[index].IsOnSurface(xx); 
+    
+}
+
+
+ILDVTrackHit* ILDPolygonBarrelMeasLayer::ConvertLCIOTrkHit( EVENT::TrackerHit* trkhit) const {
+  
+  streamlog_out(ERROR) << "ILDPolygonBarrelMeasLayer::ConvertLCIOTrkHit Not implemented: exit(1) called from " << __FILE__ << "   line " << __LINE__ << std::endl; 
+  exit(1);
+    
+}
+
+bool ILDPolygonBarrelMeasLayer::IsOutside(const TVector3 &xx) const
+{
+  int index = this->get_plane_index(xx.Phi());
+  return _planes[index].IsOutside(xx); 
+}
+
+double ILDPolygonBarrelMeasLayer::CalcS(const TVector3 &xx) const {
+
+  int index = this->get_plane_index(xx.Phi());
+  return _planes[index].CalcS(xx); 
+  
+}
+
+TMatrixD ILDPolygonBarrelMeasLayer::CalcDSDx(const TVector3 &xx) const {
+  
+  int index = this->get_plane_index(xx.Phi());
+  return _planes[index].CalcDSDx(xx); 
+  
+}
+
+int ILDPolygonBarrelMeasLayer::get_plane_index(double phi) const {
+
+  phi = angular_range_2PI(phi);
+  return (floor(phi/_segment_dphi));
+  
+}
+
+
+double ILDPolygonBarrelMeasLayer::angular_range_2PI( double phi ) const {
+  
+  //bring phi_point into range 0 < phi < +2PI
+  while (phi < 0) {
+    phi += 2.0 * M_PI;
+  }
+  while (phi >= 2.0*M_PI) {
+    phi -= 2.0 * M_PI;
+  }
+  
+  return phi;
+  
+}
+
--- ild/support/ILDSupportKalDetector.cc
+++ ild/support/ILDSupportKalDetector.cc
@@ -1,19 +1,25 @@
 
 #include "ILDSupportKalDetector.h"
 #include "ILDCylinderMeasLayer.h"
+#include "ILDPolygonBarrelMeasLayer.h"
+#include "ILDDiscMeasLayer.h"
 
 #include "TMath.h"
 #include "TTUBE.h"
+
+#include <UTIL/BitField64.h>
+#include <UTIL/ILDConf.h>
 
 #include "MaterialDataBase.h"
 
 #include <sstream>
 #include <iomanip>
+#include <cmath>
 
 #include "gear/GEAR.h"
 #include "gear/BField.h"
 #include "gearimpl/Util.h"
-
+#include "gear/CalorimeterParameters.h"
 #include "streamlog/streamlog.h"
 
 
@@ -61,6 +67,71 @@ TVKalDetector(10)
   << std::endl ;  
   
   
+  // add calo bounding inner surface as ILDPolygonBarrelMeasLayer and two planes at the inner z of the calo endcaps with ILDDiscMeasLayer
+
+  // SJA:FIXME: OK for now we will just use a set of planes as there is not yet an implementation to propagate to a multilayer
+  
+  const gear::CalorimeterParameters& ecalB = gearMgr.getEcalBarrelParameters();
+  const gear::CalorimeterParameters& ecalE = gearMgr.getEcalEndcapParameters();
+  
+  if (ecalB.getSymmetryOrder()!=8) {
+    streamlog_out(ERROR) << "ILDSupportKalDetector::ILDSupportKalDetector ECal barrel is not eightfold symmetry: exit(1) called from " << __FILE__ << "   line " << __LINE__ << std::endl; 
+    exit(1);
+
+  }
+  
+  double phi0  = ecalB.getPhi0();
+  double r_min_ecal_bar = ecalB.getExtent()[0];
+  double z_max_ecal_bar = ecalB.getExtent()[3];
+
+  UTIL::BitField64 encoder( lcio::ILDCellID0::encoder_string ) ; 
+  encoder.reset() ;  // reset to 0
+  
+  encoder[lcio::ILDCellID0::subdet] = lcio::ILDDetID::ECAL ;
+  encoder[lcio::ILDCellID0::side] = lcio::ILDDetID::barrel;
+  encoder[lcio::ILDCellID0::layer]  = 0 ;
+  
+  std::vector<int> module_ids;
+  
+  for (int i=0; i<8; ++i) {
+
+    encoder[lcio::ILDCellID0::module] = i;
+    module_ids.push_back(encoder.lowWord());
+
+    double segment_dphi = 2.0*M_PI / 8; 
+    double phi = i*segment_dphi+phi0;
+    double width = 2*(r_min_ecal_bar*tan(segment_dphi*0.5));
+    Add ( new ILDParallelPlanarMeasLayer(air,air,r_min_ecal_bar,phi,bz,r_min_ecal_bar+i*1.0e-06,width,z_max_ecal_bar,0.0,true,encoder.lowWord(),"ECalBarrelFace"));    
+    
+  }
+  
+  //  Add ( new ILDPolygonBarrelMeasLayer(air,air,bz,r_min_ecal_bar,r_min_ecal_bar,z_max_ecal_bar*0.5,8,0.0,phi0,module_ids,"ECalBarrelFace"));  
+  
+  streamlog_out( DEBUG0 )   << " *** adding ECalBarrelFace Measurement layer at Rmin = " << r_min_ecal_bar << std::endl ;
+  
+  double r_max_ecal_ecap = ecalE.getExtent()[1];
+  double z_min_ecal_ecap = ecalE.getExtent()[2];
+
+  encoder[lcio::ILDCellID0::module] = 0;
+  
+  encoder[lcio::ILDCellID0::side] = lcio::ILDDetID::fwd;
+  
+  TVector3 front_face_centre_fwd( 0.0, 0.0, z_min_ecal_ecap); // for +z  
+  TVector3 front_face_normal_fwd(front_face_centre_fwd);
+  front_face_normal_fwd.SetMag(1.0);
+  
+  Add (new ILDDiscMeasLayer(air, air, front_face_centre_fwd,front_face_normal_fwd, bz, z_min_ecal_ecap, 0., r_max_ecal_ecap/cos(M_PI/8.0), true, encoder.lowWord(),"ECalEndcapFace+Z") );
+  
+  streamlog_out( DEBUG0 )   << " *** adding ECalEndcapFace+Z Measurement layer at Zmin = " << z_min_ecal_ecap << " and Rmax = " << r_max_ecal_ecap/cos(M_PI/8.0) << std::endl ;
+  
+  encoder[lcio::ILDCellID0::side] = lcio::ILDDetID::bwd;
+  TVector3 front_face_centre_bwd( -front_face_centre_fwd ); // for -z  
+  TVector3 front_face_normal_bwd(front_face_centre_bwd);
+  front_face_normal_bwd.SetMag(1.0);
+  
+  Add (new ILDDiscMeasLayer(air, air, front_face_centre_bwd,front_face_normal_bwd, bz, z_min_ecal_ecap+1.0e-06, 0., r_max_ecal_ecap/cos(M_PI/8.0), true, encoder.lowWord(),"ECalEndcapFace-Z"));
+
+  streamlog_out( DEBUG0 )   << " *** adding ECalEndcapFace-Z Measurement layer at Zmin = " << z_min_ecal_ecap << " and Rmax = " << r_max_ecal_ecap/cos(M_PI/8.0) << std::endl ;
   
   SetOwner();
 }
