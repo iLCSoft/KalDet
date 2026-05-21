9f4f2e5654388c1bf737ee009c74260d9ae3c3fb
Juan Miguel Carceller <jmcarcell@users.noreply.github.com>
2026-05-21 10:07:38 +0200

    Comment out classes that have ClassDef commented out

--- lctpc/gearTPC/LinkDef.h
+++ lctpc/gearTPC/LinkDef.h
@@ -4,13 +4,13 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class EXTPCHit+;
-#pragma link C++ class EXTPCKalDetector+;
+//#pragma link C++ class EXTPCHit+;
+//#pragma link C++ class EXTPCKalDetector+;
 #pragma link C++ class EXTPCMeasLayer+;
 #pragma link C++ class kaldet::GearTPCHit+;
-#pragma link C++ class kaldet::GearTPCCylinderHit+;
-#pragma link C++ class kaldet::GearTPCCylinderMeasLayer+;
-#pragma link C++ class kaldet::GearTPCKalDetector+;
+//#pragma link C++ class kaldet::GearTPCCylinderHit+;
+//#pragma link C++ class kaldet::GearTPCCylinderMeasLayer+;
+//#pragma link C++ class kaldet::GearTPCKalDetector+;
 #pragma link C++ class kaldet::GearTPCMeasLayer+;
 
 #endif
